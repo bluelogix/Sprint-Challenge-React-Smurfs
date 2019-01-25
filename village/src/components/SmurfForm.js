a3d8d2a886f7423443555ced6dd050f5a6d6e204
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-25 12:19:31 -0500

    intial commit

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+          name: '',
+          age: '',
+          height: ''
+          
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios.post('http://localhost:3333/smurfs')
+    .then(res => {
     this.setState({
+      smurfs: res.data,
       name: '',
       age: '',
       height: ''
     });
+  })
   }
 
   handleInputChange = e => {
